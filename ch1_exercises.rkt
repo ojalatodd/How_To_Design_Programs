0be4d416260eda3afa67fe556828c74646b79af5
Todd Ojala <ojalatodd@users.noreply.github.com>
2024-12-18 15:45:05 -0600

    Update ch1_exercises.rkt
    Added exercise 4

--- ch1_exercises.rkt
+++ ch1_exercises.rkt
@@ -4,3 +4,8 @@
 (define i 5)
 
 (string-append (substring str 0 i) "_" (substring str i (string-length str)))
+
+; Chapter 1, Exercise 4
+; Delete the ith position from string
+(string-append (substring str 0 i) (substring str (+ 1 i) (string-length str)))
+
